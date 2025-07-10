675d5a85a7693ca2245519b747343ba675bced82
Erin Millard <contact@erin.id.au>
2025-07-10 23:40:16 +1000

    Increase test timeouts under CI

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,13 +1,15 @@
 import tsconfigPaths from "vite-tsconfig-paths";
 import { defineConfig } from "vitest/config";
 
+const isCI = process.env.CI === "true";
+
 export default defineConfig({
   plugins: [tsconfigPaths()],
   test: {
     watch: false,
-    testTimeout: 1000,
-    hookTimeout: 1000,
-    teardownTimeout: 1000,
+    testTimeout: isCI ? 10000 : 1000,
+    hookTimeout: isCI ? 10000 : 1000,
+    teardownTimeout: isCI ? 10000 : 1000,
     include: ["test/vitest/**/*.spec.ts"],
     coverage: {
       include: ["src/**/*.ts", "artifacts/dist/loader/*.js"],
